81dfcb45fffa72ef02df8be38af0a1262454b839
CarJimCha <carlosjimcha@gmail.com>
2025-04-10 11:04:32 +0200

    Added basic login

---
+++ src/Entity/Transaction.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TransactionRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TransactionRepository::class)]
+class Transaction
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $cantidad = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $precio = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $timestamp = null;
+
+    #[ORM\ManyToOne(inversedBy: 'transaction')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user_id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'transaction')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Item $item_id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCantidad(): ?int
+    {
+        return $this->cantidad;
+    }
+
+    public function setCantidad(int $cantidad): static
+    {
+        $this->cantidad = $cantidad;
+
+        return $this;
+    }
+
+    public function getPrecio(): ?int
+    {
+        return $this->precio;
+    }
+
+    public function setPrecio(?int $precio): static
+    {
+        $this->precio = $precio;
+
+        return $this;
+    }
+
+    public function getTimestamp(): ?\DateTimeInterface
+    {
+        return $this->timestamp;
+    }
+
+    public function setTimestamp(\DateTimeInterface $timestamp): static
+    {
+        $this->timestamp = $timestamp;
+
+        return $this;
+    }
+
+    public function getUserId(): ?User
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(?User $user_id): static
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    public function getItemId(): ?Item
+    {
+        return $this->item_id;
+    }
+
+    public function setItemId(?Item $item_id): static
+    {
+        $this->item_id = $item_id;
+
+        return $this;
+    }
+}
