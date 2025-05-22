7fc05a652dea3baa02e8c6fb0d89bfd0a8119038
CarJimCha <carlosjimcha@gmail.com>
2025-05-22 19:59:38 +0200

    Aesthetic changes: buttons

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,7 +10,6 @@ class HomeController extends AbstractController
     #[Route('/', name: 'index')]
     public function index()
     {
-        // Aquí puedes traer los objetos de la base de datos, pero por ahora lo dejamos simple
         return $this->render('index.html.twig');
     }
 }
